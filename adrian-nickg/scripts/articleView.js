3f11f12cc94c1c6c47c00e4668fce38a5ef4d2c8
AQHawk <ironhawkcreations@gmail.com>
2017-10-27 13:41:25 -0700

    finished comments

--- adrian-nickg/scripts/articleView.js
+++ adrian-nickg/scripts/articleView.js
@@ -2,10 +2,10 @@
 
 let articleView = {};
 
-// TODO: Where possible, refactor methods into arrow functions, including the document.ready() method at the bottom.
+// Done: Where possible, refactor methods into arrow functions, including the document.ready() method at the bottom.
 
 // COMMENT: How do arrow functions affect the context of "this"? How did you determine if a function could be refactored?
-// PUT YOUR RESPONSE HERE
+// We only targeted the outside functions. The nested functions we left as is. 
 
 articleView.populateFilters = (() => {
   $('article').each(function() {
